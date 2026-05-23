9d4f9019e8cb5879065d9de175b88713605f2069
Даниил <daniil.kamalov2001@mail.ru>
2026-05-23 17:36:15 +0300

    favicon static server

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const start = async () => {
     try {
         await sequelize.authenticate();
         await sequelize.sync();
-        app.listen(PORT, () => console.log(`Server succesfully started on port ${PORT}`));
+        app.listen(PORT, '0.0.0.0', () => console.log(`Server succesfully started on port ${PORT}`));
     } catch(e) {
         console.log(e)
     }
